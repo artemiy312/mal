a227e0085c7084b53341a3bc13c99cc214137e27
Artemiy Rodionov <wertins71@gmail.com>
2021-05-06 19:17:21 +0300

    Implement required functions for step1

--- impls/moonlisp/test_printer.lua
+++ impls/moonlisp/test_printer.lua
@@ -14,12 +14,12 @@ test_nil = make_test(t.Nil(), "nil")
 test_true = make_test(t.Boolean(true), "true")
 test_false = make_test(t.Boolean(false), "false")
 test_symbol = make_test(t.Symbol("abc"), "abc")
-test_list_empty = make_test(t.List(), "()")
+test_list_empty = make_test(t.List({}), "()")
 test_list_atom = make_test(
-  t.List(
+  t.List({
     t.Number(1),
     t.Symbol("abc"),
-    t.Boolean(false)),
+    t.Boolean(false)}),
   "(1 abc false)")
 
 os.exit(lu.LuaUnit.run())
